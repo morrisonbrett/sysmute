89b1e543a253ab4676344fabc4ee9168058e9a50
Brett Morrison <b@brettmorrison.com>
2017-02-23 18:32:06 -0800

    Added command line arg processing to override time window default start / end. Added supplemental files to solution Improved readme

--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
-## Sysmute
+## sysmute
 
-This program was built to silence the volume during a wide window of sleeping hours.
+This program was built to silence the volume during a window of sleeping hours.
 It came about because I would forget to turn the speakers off or mute at night,
 and during the quietness of the night, alerts arriving in throughout the night with audible alerts
 would disturb my family's sleep. Windows 10 doesn't currently have a built-in "volume silence" time window,
 so I wrote this short program to do it.
 
-On my desktop machine, I added a task in Windows Task Scheduler to run 'At System Startup'.
+On my [desktop machine](https://www.flickr.com/photos/morrisonbrett/19799056499), I added a task in Windows Task Scheduler to run 'At System Startup'.
 
-The program will mute the volume during fixed hours.  The time window is a constant in the C# code.
+The program will mute the volume during fixed hours.  Default time window is 10:00pm -> 8:00am.
+This can be overridden by passing in start time / end time on the command line.  For example, `> sysmute.exe 23:00 07:30`.
 
 If user manually unmutes, it stays unmuted.
--- sysmute/Program.cs
+++ sysmute/Program.cs
@@ -18,17 +18,76 @@ namespace sysmute
 
             return start_time_of_day <= time_of_day || time_of_day <= end_time_of_day;
         }
+
+        public static int Hour(this string timeString)
+        {
+            try
+            {
+                var hour = Int32.Parse(timeString.Substring(0, timeString.IndexOf(":", StringComparison.Ordinal)));
+
+                return hour;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Invalid hour {timeString}.  Must use HH:MM");
+                throw e;
+            }
+        }
+
+        public static int Minute(this string timeString)
+        {
+            try
+            {
+                var minute = Int32.Parse(timeString.Substring(timeString.IndexOf(":", StringComparison.Ordinal) + 1));
+
+                return minute;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Invalid minute {timeString}.  Must use HH:MM");
+                throw e;
+            }
+        }
     }
 
     class Program
     {
-        public static readonly DateTime startTime = new DateTime(1969, 2, 25, 22, 00, 00); // Date doesn't matter. 10pm
-        public static readonly DateTime endTime = new DateTime(1969, 2, 26, 9, 00, 00); // Date doesn't matter. 9am
+        public static DateTime startTime = new DateTime(1969, 2, 25, 22, 00, 00); // Date doesn't matter. 10pm
+        public static DateTime endTime = new DateTime(1969, 2, 26, 9, 00, 00); // Date doesn't matter. 9am
         public static readonly int SleepInterval = 1000 * 60; // Check the time every 1 minute
 
         static void Main(string[] args)
         {
+            // Check the command args.  If set, override the default
+            if (args.Length > 0 && args[0] != null)
+            {
+                try
+                {
+                    startTime = new DateTime(1969, 2, 25, args[0].Hour(), args[0].Minute(), 00);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid start time");
+                    return;
+                }
+            }
+
+            if (args.Length > 1 && args[1] != null)
+            {
+                try
+                {
+                    endTime = new DateTime(1969, 2, 26, args[1].Hour(), args[1].Minute(), 00);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid end time");
+                    return;
+                }
+            }
+
             Console.WriteLine($"sysmute. Program will mute system audio between {startTime.TimeOfDay} and {endTime.TimeOfDay}");
+            if (args.Length == 0)
+                Console.WriteLine($"To override startTime and endTime, pass in via command line. E.g. > sysmute 23:00 08:00");
 
             var needToMute = true; // Controls to not mute if already muted.
 
